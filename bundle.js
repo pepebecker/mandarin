d3b9051c8836ef0ef09b1331c5fd9887d0f399bb
Pepe Becker <mail@pepebecker.com>
2018-05-09 19:33:24 +0100

    Update

--- bundle.js
+++ bundle.js
@@ -1,18 +1,3 @@
-(function () {
-  var socket = document.createElement('script')
-  var script = document.createElement('script')
-  socket.setAttribute('src', 'http://127.0.0.1:1337/socket.io/socket.io.js')
-  script.type = 'text/javascript'
-
-  socket.onload = function () {
-    document.head.appendChild(script)
-  }
-  script.text = ['window.socket = io("http://127.0.0.1:1337");',
-  'socket.on("bundle", function() {',
-  'console.log("livereaload triggered")',
-  'window.location.reload();});'].join('\n')
-  document.head.appendChild(socket)
-}());
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const utils = require('pinyin-utils')
 
@@ -139,7 +124,6 @@ hanziBtn.addEventListener('click', () => setMode('hanzi', true))
 
 inputField.addEventListener('input', convert)
 
-inputField.value = '我的貓喜歡喝牛奶'
 convert()
 
 },{"pinyin-utils":2}],2:[function(require,module,exports){
